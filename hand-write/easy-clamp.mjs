346e9723c50c14bcfffe987a36970821e9d0c2c2
sunpochin <sunpochin@gmail.com>
2025-04-14 22:34:35 +0800

    daily check

---
+++ hand-write/easy-clamp.mjs
@@ -0,0 +1,29 @@
+// import lodash
+
+export function clamp(number, lower, upper) {
+  return Math.max(lower, Math.min(number, upper));
+}
+
+// function clamp(number, lower, upper) {
+//   if (number < lower) {
+//     return lower;
+//   } else if (number > upper) {
+//     return upper;
+//   } else {
+//     return number;
+//   }
+// }
+
+// touch and go
+// inside turn, outside turn
+// sweetheart
+// TPO
+
+// 範圍中，返回原值
+console.log(clamp(7, 0, 9)) // => 7
+
+// 小於 lower，返回 lower
+console.log(clamp(-12, -4, 5)) // => -4
+
+// 大於 upper，返回 upper
+console.log(clamp(18, 3, 9)) // => 9
